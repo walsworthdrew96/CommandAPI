2a1a191e962f016d28061eb1f2b5a8428215ed81
walsworthdrew96 <walsworth.drew96@gmail.com>
2020-11-24 14:15:39 -0600

    Added CommandContext to solution

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -3,19 +3,29 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration {get;}
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("PostgreSqlConnection")));
             //register services to enable the use of "Controllers" throughout the application.
             services.AddControllers();
-            //this configuration creates one MockCommandAPIRepo per request for one.
-            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            // //this configuration creates one MockCommandAPIRepo per request for one.
+            // services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+            // add the real SqlCommandAPIRepo as a service
+            services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
